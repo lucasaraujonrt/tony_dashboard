5dfff680951e06fd2d939b345a64505e0c8de9e6
juliap <juliap@ciandt.com>
2021-09-27 15:23:42 -0300

    adding circle icons and layout to kanban board

--- src/app/pages/Kanban/Kanban/KanbanDetails.tsx
+++ src/app/pages/Kanban/Kanban/KanbanDetails.tsx
@@ -4,6 +4,9 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { translate } from '@portal/services/i18n';
 import { Divider } from 'antd';
 import Card from '@portal/components/Card/Card';
+import IconBlueCircle from '~/assets/svg/ic_blue_circle.svg';
+import IconYellowCircle from '~/assets/svg/ic_yellow_circle.svg';
+import IconGreenCircle from '~/assets/svg/ic_green_circle.svg';
 
 const KanbanDetails: React.FC = () => {
   return (
@@ -18,8 +21,13 @@ const KanbanDetails: React.FC = () => {
       </Row>
       <Divider />
       <Row>
-        <Col md={3} className="kanban__inner__title">
+        <Col className="kanban__inner__title">
           Criados
+          <img
+            src={IconBlueCircle}
+            alt="Icone círculo criado"
+            className="kanban__inner__icon"
+          />
           <div className="kanban__inner__board">
             <Card
               description="Problema no encanamento da pia daquele apartamento"
@@ -37,8 +45,17 @@ const KanbanDetails: React.FC = () => {
           </div>
         </Col>
 
-        <Col md={3} className="kanban__inner__title">
+        <Col className="kanban__inner__title">
+          <Divider className="kanban__divider" type="vertical" />
+        </Col>
+
+        <Col className="kanban__inner__title">
           Em progresso
+          <img
+            src={IconYellowCircle}
+            alt="Icone círculo criado"
+            className="kanban__inner__icon"
+          />
           <div className="kanban__inner__board">
             <Card
               description="Problema no encanamento da pia daquele apartamento"
@@ -49,9 +66,43 @@ const KanbanDetails: React.FC = () => {
           </div>
         </Col>
 
-        <Col md={3} className="kanban__inner__title">
+        <Col className="kanban__inner__title">
+          <Divider className="kanban__divider" type="vertical" />
+        </Col>
+
+        <Col className="kanban__inner__title">
           Finalizados
+          <img
+            src={IconGreenCircle}
+            alt="Icone círculo criado"
+            className="kanban__inner__icon"
+          />
           <div className="kanban__inner__board">
+            <Card
+              description="Problema no encanamento da pia daquele apartamento"
+              priority="Alta"
+              sector="Encanamento"
+              createdAt="Criado em 22 de setembro"
+            />
+
+            <Card
+              description="Problema no encanamento da pia daquele apartamento"
+              priority="Alta"
+              sector="Encanamento"
+              createdAt="Criado em 22 de setembro"
+            />
+            <Card
+              description="Problema no encanamento da pia daquele apartamento"
+              priority="Alta"
+              sector="Encanamento"
+              createdAt="Criado em 22 de setembro"
+            />
+            <Card
+              description="Problema no encanamento da pia daquele apartamento"
+              priority="Alta"
+              sector="Encanamento"
+              createdAt="Criado em 22 de setembro"
+            />
             <Card
               description="Problema no encanamento da pia daquele apartamento"
               priority="Alta"
